04b9e4a8b8a72a3bdc22ad1b21fcf8c0e95c1c14
isombyt@isombyt.me <isombyt@isombyt.me>
2012-07-24 12:22:25 +0800

    finish bot-format.py and bot-example.py

--- LocalControl/bot-example.py
+++ LocalControl/bot-example.py
@@ -1,33 +1,44 @@
-# -*- coding: utf-8 -*-
 
-def default(rawString,memberID):
-    comment="@%s \xe3\x81\x86\xe3\x82\x8b\xe3\x81\x95\xe3\x81\x84"%memberID
+def defaultHandler(rawString,memberID):
+    "default handler"
+    comment="@%s no rule match"%memberID #way to mention the member who had mention you
     return comment
 
-def Dice(rawString,memberID):
+def DiceHandler(rawString,memberID):
+    "handles the rule '(\d+)d(\d+)'"
     import re
     import random
-    result=re.findall("(\d+)d(\d+)",rawString)
+    "import must write inside every functions which need it"
+    result=re.findall("(\d+)d(\d+)",rawString)#get the re match result
     if len(result)>0:
         num=int(result[0][0])
         max=int(result[0][1])
         if num==0 or max==0:
-            comment=='\xe4\xbd\xa0\xe8\x80\x8d\xe8\x80\x81\xe5\xa8\x98\xe6\x98\xaf\xe4\xb8\x8d'
+            comment=='error input'
         else:
             lst=[str(random.randint(0,max)) for i in range(num)]
-            comment='%s\xe5\x8f\xaa\xe9\xaa\xb0\xe5\xad\x90\xe8\xbd\xac\xe5\x87\xba%s'%(num,'\xe3\x80\x81'.join(lst))
-        comment="@%s %s"%(memberID,comment)
+            comment='%sdices roled %s'%(num,'\xe3\x80\x81'.join(lst))
+        comment="@%s %s"%(memberID,comment)#add a mention
         return comment
     return None
         
 
-bot=[["default",default],
-     ["(\d+)d(\d+)",Dice],]
+bot=[["default",defaultHandler],#make function defaultHandler default
+     ["(\d+)d(\d+)",DiceHandler],]#handle '(\d+)d(\d+)' with function DiceHandler
 
+
+
+
+
+
+"""
+some local test code,copied from server/ParseNotification.py to test the bot
+forget it if you cant understand it
+"""
 import re
 
 if __name__=="__main__":
-    rawString='<span class="proflinkWrapper"><span class="proflinkPrefix">+</span><a href="https://plus.google.com/100974053404896066917" class="proflink" oid="100974053404896066917">bit Huang</a></span> 1024d1024﻿'
+    rawString='1024d1024'
     memberID="233233233"
     comment=None
     for single in bot:
